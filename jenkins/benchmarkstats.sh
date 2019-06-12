2d29cc63d9aec2b1bb9eaf570a2ae9d6abdcc394
Benjamin Hodges <benjaminhodges07@gmail.com>
2019-06-12 15:19:18 +0100

    Fix column names

--- jenkins/benchmarkstats.sh
+++ jenkins/benchmarkstats.sh
@@ -4,8 +4,11 @@ FILE="benchmarkdb.csv"
 
 if [ ! -f $FILE ]; then
 	touch $FILE
+else
+	rm $FILE
 fi
 
+echo "Date, Read, Write" >> $FILE
 echo -n "`date +%D`" >> $FILE 
 
 ./benchmarkmockup.sh | tr -d "[:alpha:] [:blank]" |
